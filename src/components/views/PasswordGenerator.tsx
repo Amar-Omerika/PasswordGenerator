b00deb394823c0f8ea3ecd7ab8bf84647b320280
Amar-Omerika <72925790+Amar-Omerika@users.noreply.github.com>
2024-08-18 16:52:38 +0200

    feat: add some animation for button and inputs

--- src/components/views/PasswordGenerator.tsx
+++ src/components/views/PasswordGenerator.tsx
@@ -5,6 +5,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Checkbox from "../ui/Checkbox";
 import { CheckboxState } from "../../types/types";
+import { AnimatePresence, motion } from "framer-motion";
 
 function PasswordGenerator() {
   const [length, setLength] = useState<number>(10);
@@ -72,75 +73,95 @@ function PasswordGenerator() {
   };
 
   return (
-    <div className="password-generator">
-      <ToastContainer />
-      <div className="password-generator__input-wrapper">
-        <input
-          type="text"
-          value={password}
-          readOnly
-          className="password-generator__input"
-        />
-        <FaCopy
-          onClick={handleCopyToClipboard}
-          className="password-generator__copy-icon"
-          title="Copy to clipboard"
-        />
-      </div>
-      <div className="password-generator__controls">
-        <label>Character length {length}</label>
-        <input
-          type="range"
-          min="1"
-          max="20"
-          value={length}
-          onChange={(e) => setLength(Number(e.target.value))}
-          className="password-generator__slider"
-        />
-      </div>
-      <div className="password-generator__checkbox-group">
-        <Checkbox
-          label="Include Lowercase"
-          checked={checkboxState.includeLowercase}
-          onChange={() =>
-            handleCheckboxChange(
-              "includeLowercase",
-              checkboxState.includeLowercase
-            )
-          }
-        />
-        <Checkbox
-          label="Include Uppercase"
-          checked={checkboxState.includeUppercase}
-          onChange={() =>
-            handleCheckboxChange(
-              "includeUppercase",
-              checkboxState.includeUppercase
-            )
-          }
-        />
-        <Checkbox
-          label="Include Numbers"
-          checked={checkboxState.includeNumbers}
-          onChange={() =>
-            handleCheckboxChange("includeNumbers", checkboxState.includeNumbers)
-          }
-        />
-        <Checkbox
-          label="Include Symbols"
-          checked={checkboxState.includeSymbols}
-          onChange={() =>
-            handleCheckboxChange("includeSymbols", checkboxState.includeSymbols)
-          }
-        />
-      </div>
-      <button
-        onClick={handleGeneratePassword}
-        className="password-generator__button"
+    <AnimatePresence>
+      <motion.div
+        className="overflow-hidden"
+        initial={{ opacity: 0, height: 0 }}
+        animate={{ opacity: 1, height: "auto" }}
+        exit={{ opacity: 0, height: 0 }}
+        transition={{ duration: 1.5 }}
       >
-        Generate
-      </button>
-    </div>
+        <div className="password-generator">
+          <ToastContainer />
+          <div className="password-generator__input-wrapper">
+            <motion.input
+              type="text"
+              value={password}
+              readOnly
+              className="password-generator__input"
+              key={password}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
+            />
+            <FaCopy
+              onClick={handleCopyToClipboard}
+              className="password-generator__copy-icon"
+              title="Copy to clipboard"
+            />
+          </div>
+          <div className="password-generator__controls">
+            <label>Character length {length}</label>
+            <input
+              type="range"
+              min="1"
+              max="20"
+              value={length}
+              onChange={(e) => setLength(Number(e.target.value))}
+              className="password-generator__slider"
+            />
+          </div>
+          <div className="password-generator__checkbox-group">
+            <Checkbox
+              label="Include Lowercase"
+              checked={checkboxState.includeLowercase}
+              onChange={() =>
+                handleCheckboxChange(
+                  "includeLowercase",
+                  checkboxState.includeLowercase
+                )
+              }
+            />
+            <Checkbox
+              label="Include Uppercase"
+              checked={checkboxState.includeUppercase}
+              onChange={() =>
+                handleCheckboxChange(
+                  "includeUppercase",
+                  checkboxState.includeUppercase
+                )
+              }
+            />
+            <Checkbox
+              label="Include Numbers"
+              checked={checkboxState.includeNumbers}
+              onChange={() =>
+                handleCheckboxChange(
+                  "includeNumbers",
+                  checkboxState.includeNumbers
+                )
+              }
+            />
+            <Checkbox
+              label="Include Symbols"
+              checked={checkboxState.includeSymbols}
+              onChange={() =>
+                handleCheckboxChange(
+                  "includeSymbols",
+                  checkboxState.includeSymbols
+                )
+              }
+            />
+          </div>
+          <button
+            onClick={handleGeneratePassword}
+            className="password-generator__button"
+          >
+            Generate
+          </button>
+        </div>
+      </motion.div>
+    </AnimatePresence>
   );
 }
 
